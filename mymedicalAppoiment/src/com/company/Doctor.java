59d4a60a3a6bf9097e1bbaf38d4bde4eb9e40ec3
Wilson Aceros <wilson.acerosc@gmail.com>
2020-11-04 21:50:00 -0500

    Curso plazti java POO variables vs objetos 04 nov 2020

--- mymedicalAppoiment/src/com/company/Doctor.java
+++ mymedicalAppoiment/src/com/company/Doctor.java
@@ -2,12 +2,13 @@ package com.company;
 
 public class Doctor {
     //Atributos
-    int id;
+    static int id = 0; //Autoincrement
     String name;
     String speciality;
 
     Doctor(){
         System.out.println("Construyendo el Objeto Doctor");
+        id++;
     }
 
     Doctor(String name){
@@ -17,5 +18,9 @@ public class Doctor {
     //Comportamientos
     public void showName(){
         System.out.println(name);
+    }
+
+    public void showId(){
+        System.out.println("ID Doctor: "+id);
     }
 }
--- mymedicalAppoiment/src/com/company/Main.java
+++ mymedicalAppoiment/src/com/company/Main.java
@@ -1,11 +1,47 @@
 package com.company;
+import static UI.UIMenu.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        /*
         Doctor myDoctor = new Doctor();
         myDoctor.name = "Alejandro Rodriguez";
         myDoctor.showName();
+        myDoctor.showId();
+        System.out.println(Doctor.id);
+
+        Doctor.id++;
+
+        Doctor myDoctorAnn = new Doctor();
+        myDoctorAnn.showId();
+        System.out.println(Doctor.id);
+        */
+
+        //showMenu();
+
+        int i=0;
+        int b=2;
+        b=i;
+
+        String name = "Wilson";
+
+        Patient patient = new Patient("Wilson", "wilson.acerosc@gmail.com");
+        Patient patient2 = new Patient("Marce", "marce.sanchez@gmail.com");
+        System.out.println(patient);
+        System.out.println(patient2);
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2=patient;
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
     }
+
+
 }
